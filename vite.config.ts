2f33a0ee61039c41780ef040aea292b243d62f08
common-gull <common-gull.elastic742@passmail.net>
2025-03-01 19:13:38 -0500

    Add publish workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const ENVIRONMENT = process.env['ENVIRONMENT'];
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: ENVIRONMENT === 'prod' ? 'squirrel-time-tracking' : '/',
+});
