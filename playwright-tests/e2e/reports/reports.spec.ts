8f65323b8775a07d34076a2fd0a63278461c87b6
common-gull <common-gull.elastic742@passmail.net>
2025-03-09 15:14:49 -0400

    test: drop snapshots

--- playwright-tests/e2e/reports/reports.spec.ts
+++ playwright-tests/e2e/reports/reports.spec.ts
@@ -24,30 +24,55 @@ async function setup(page: Page) {
     await navigateTo(page, links.reports);
 }
 
-function getScreenshotOpts(page: Page) {
-    return { mask: [page.locator('header'), page.locator('nav')] };
+function tableDataToLongString(tableData: string[]) {
+    return tableData.join('').replace(/,/g, '');
 }
 
-test('Screenshot shows expected data with default settings set', async ({ page }) => {
+test('Displays expected table data with default settings', async ({ page }) => {
     await setup(page);
-    await expect(page).toHaveScreenshot(getScreenshotOpts(page));
+    const tableData = [
+        'name,2025-02-18,2025-02-19,2025-02-20,2025-02-21,2025-02-22,2025-02-23,2025-02-24,2025-02-25',
+        'Investigate network connectivity issues,0.00,0.00,0.00,0.00,2.00,0.00,0.00,0.00',
+        'Prepare for impromptu meeting,0.00,0.00,0.00,0.00,0.68,0.00,0.00,0.00',
+        'Meeting with client,0.00,0.00,0.00,0.00,1.00,0.00,0.00,0.00',
+        'Create UI mocks for client,0.00,0.00,0.00,0.00,0.00,1.00,0.00,0.00',
+        'Email client,0.00,0.00,0.00,0.00,0.00,0.25,0.00,0.00',
+        'System design,0.00,0.00,0.00,0.00,0.00,1.75,0.00,0.00',
+        'Lunch,0.00,0.00,0.00,0.00,0.00,0.75,0.00,0.00',
+    ];
+    await expect(page.getByRole('table')).toContainText(tableDataToLongString(tableData));
 });
 
-test('Screenshot shows expected data with default range and group by set to project', async ({
+test('Displays expected table data with default range and group by set to project', async ({
     page,
 }) => {
     await setup(page);
     await page.getByLabel(settings.groupBy).selectOption(settings.project);
     await page.getByRole('button', { name: settings.update }).click();
-    await expect(page).toHaveScreenshot(getScreenshotOpts(page));
+
+    const tableData = [
+        'project,2025-02-18,2025-02-19,2025-02-20,2025-02-21,2025-02-22,2025-02-23,2025-02-24,2025-02-25',
+        'No Project,0.00,0.00,0.00,0.00,2.00,0.75,0.00,0.00',
+        'P-1234,0.00,0.00,0.00,0.00,1.68,1.25,0.00,0.00',
+        'P-5678,0.00,0.00,0.00,0.00,0.00,1.75,0.00,0.00',
+    ];
+    await expect(page.getByRole('table')).toContainText(tableDataToLongString(tableData));
 });
 
-test('Screenshot shows expected data with custom settings', async ({ page }) => {
+test('Displays expected table data with custom settings', async ({ page }) => {
     await setup(page);
     await page.getByRole('button', { name: settings.dateRange }).click();
+
     await page.getByRole('button', { name: '18 February 2025', exact: true }).click();
     await page.getByRole('button', { name: '22 February 2025', exact: true }).click();
+
     await page.getByLabel(settings.groupBy).selectOption(settings.project);
     await page.getByRole('button', { name: settings.update }).click();
-    await expect(page).toHaveScreenshot(getScreenshotOpts(page));
+
+    const tableData = [
+        'project,2025-02-18,2025-02-19,2025-02-20,2025-02-21,2025-02-22',
+        'No Project,0.00,0.00,0.00,0.00,2.00',
+        'P-1234,0.00,0.00,0.00,0.00,1.68',
+    ];
+    await expect(page.getByRole('table')).toContainText(tableDataToLongString(tableData));
 });
