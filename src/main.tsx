1c1d5c97450827a44a6fb007f9af8095bb81b7da
common-gull <common-gull.elastic742@passmail.net>
2025-07-12 12:01:07 -0400

    refactor: replace mantine-react-table with custom MantineTable component

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App.tsx';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/notifications/styles.css';
-import 'mantine-react-table/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { mantineTheme } from './theme.ts';
